42ee7457382d51884bedbb5ee87eff21cadadc2c
gayeon337 <gayeon337.lee@lge.com>
2026-06-25 10:12:21 +0900

    feat: add settings window frontend with routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SettingsWindow from "./components/SettingsWindow";
 import { SettingsProvider } from "./context/SettingsContext";
+
+const isSettingsView = new URLSearchParams(window.location.search).get("view") === "settings";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <SettingsProvider>
-      <App />
+      {isSettingsView ? <SettingsWindow /> : <App />}
     </SettingsProvider>
   </React.StrictMode>,
 );
